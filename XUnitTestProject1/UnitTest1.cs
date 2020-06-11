c2e1a34ddaed2b5747cf8019a97f2793da32917b
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-06-11 16:23:17 +0200

    Create unit test is added

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Moq;
 using ProductApplication;
 using ProductApplication.Controller;
@@ -15,47 +16,84 @@ namespace ProductApplicationTestProject
 {
     public class UnitTest1
     {
-        ProductRepository repository;
-        Mock<IProductRepository> repositoryMock;
-        Mock<Product> productMock;
-        private object result;
+
+        // Product
+        // create Product-3 testcases-Indira
+        //read Product-3 testcases-Tatjana
+        //update Product-3 testcase-Abde
+        //delete Product -4 testcase-Indira
+        
+            //Create Product
 
         [Fact]
-        public void SetUp()
+        public void CreateProductSuccessValidation()
         {
-            repositoryMock = new Mock<IProductRepository>();
-            repository = new ProductRepository();
-            productMock = new Mock<Product>();
+                      
+          ProductManagement productManagement = new ProductManagement();
+          productManagement.CreateProduct();
+          ProductRepository productRepository = new ProductRepository();
+          var result1 =  productRepository.GetAllProducts();
+          Assert.True(result1.Count() == 22,"No of Product count is not matched");
+             
+
+        }
+
+
+        [Fact]
+        public void CreateProductFailureValidation()
+        {
+            ProductManagement productManagement = new ProductManagement();
+            productManagement.CreateProduct();
+            ProductRepository productRepository = new ProductRepository();
+            var result2 = productRepository.GetAllProducts();
+            Assert.True(result2.Count() == 21, "No of Product count is not matched");
+
+
+        }
+
+        [Fact]
+       
+        public void CreateProductException()
+        {
+          var productManagement = new ProductManagement();
+                     
+            Assert.Throws<FileNotFoundException>(() => productManagement.CreateProduct());
+            
+        }
+
+
+        [Fact]
+        public void DeleteProductSuccessValidation()
+        {
+
+        }
+
+        [Fact]
+        public void DeleteProductFailureValidation()
+        { 
+        
+        }
+
+        [Fact]
+        public void DeleteProductException()
+        {
 
         }
 
 
 
-        [Fact]
-        public void ShouldNotSaveInvalidProduct()
-        {
-
-            var records = new List<Product>
-            {
-             new Product() { Name = "SoyaBeans", Price = 92.89m, ProductId = 324, ProductInStock = 10, ManufacturerDetails = new Manufacturer() { ManufacturerName = "III", Place = "Hjlmar", PhoneNumber = 761234567 } },
-             new Product() { Name = "Chocolate", Price = 34.85m, ProductId = 453, ProductInStock = 5, ManufacturerDetails = new Manufacturer() { ManufacturerName = "AAA", Place = "Redbergsplatsen", PhoneNumber = 762345678 } }
-
-            };
-
-
-            productMock.SetupGet(p => p.IsValid).Returns(false);
-             bool result=repository.SaveProduct(productMock.Object);
-
-
-            Assert.False(result);
-            ///repositoryMock.Verify(r => r.SaveProduct(It.IsAny<Product>()), Times.Never());
 
 
 
-        }
+
+
+
+
+
+
 
 
     }
 
-    }
+}
 
