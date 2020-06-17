79707b0d33673d104b4e1ec43d20d0bbc780b2da
Group1-IAT <66618842+Group1-IAT@users.noreply.github.com>
2020-06-17 23:55:42 +0200

    Created MongoDBProduct related code

--- ProductApplication/Program.cs
+++ ProductApplication/Program.cs
@@ -6,6 +6,10 @@ using System.Globalization;
 using System.IO;
 using ProductApplication;
 using ProductApplication.Controller;
+using MongoDB.Driver;
+using ProductApplication.Models;
+using ProductApplication.Repositories;
+using ProductApplication.MongoDb_Models;
 
 namespace ProductApplication
 {
@@ -13,13 +17,27 @@ namespace ProductApplication
     {
         static void Main(string[] args)
         {
-            //Product Management
-            ProductManagement prod = new ProductManagement();
-            prod.ProdManagment();
 
-            //Store Management
-            StoreManagement store = new StoreManagement();
-            store.StoresManagment();
+            #region JsonFileOperations
+            ////Product Management
+            //ProductManagement prod = new ProductManagement();
+            //prod.ProdManagment();
+
+            ////Store Management
+            //StoreManagement store = new StoreManagement();
+            //store.StoresManagment();
+            #endregion
+
+            #region MongoDbOperations
+
+            IMongoDbProductRepository mongoRep = new MongoDbProductRepository();
+            IEnumerable<MongoProduct> productList = mongoRep.GetAllProducts();
+            
+
+            mongoRep.InsertProduct("Products",new MongoProduct { ProductId= "5eea1bf08ad05d339877dee3", Name="Milk",Price=123,ProductInStock=12});
+            #endregion
+
+
         }
     }
 }
---
+++ ProductApplication/Repositories/MongoDbProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic;
+using MongoDB.Driver;
+using ProductApplication.Models;
+using ProductApplication.MongoDb_Models;
+
+namespace ProductApplication.Repositories
+{
+    public class MongoDbProductRepository : IMongoDbProductRepository
+    {
+
+
+        private MongoClient client;
+            private IMongoDatabase database;
+            private IMongoCollection<MongoProduct> collection;
+ 
+        public MongoDbProductRepository()
+        {
+
+             client = new MongoClient("mongodb://localhost:27017");
+             database = client.GetDatabase("Warehouse");
+             collection = database.GetCollection<MongoProduct>("Products");
+
+
+        }
+
+        public IEnumerable<MongoProduct> GetAllProducts()
+        {
+            var allProducts = collection.Find(Builders<MongoProduct>.Filter.Empty);
+            return allProducts.ToEnumerable();
+        }
+
+        
+
+        public MongoProduct GetByProductId(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertProduct(string tableName, MongoProduct product)
+        {
+              var collection = database.GetCollection<MongoProduct>(tableName);
+            collection.InsertOne(product);
+        }
+
+        public string RemoveProduct(MongoProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public void SaveProduct(List<MongoProduct> products)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public string UpdateProduct(MongoProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        
+    }
+}
